e0b80acef21b414f8a0dc41a00d80eb3ef31cf12
Khoa Hoang Hoa <khoa.hoang@duluxgroup.com>
2024-09-06 14:48:14 +0700

    Test github

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, my name is Kay! Testing the Review step'));
+app.get('/', (req, res) => res.send('Hello, my name is Kay! Testing the Review step. Approve from github'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
